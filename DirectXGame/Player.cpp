975467269309c28c6fb176cbe3cb7e9ad9467f6e
WadaKeita <k023g0069@g.neec.ac.jp>
2024-04-23 23:41:49 +0900

    03_03_確認課題

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,7 +1,7 @@
 #include "Player.h"
-#include <cassert>
 #include "Function.h"
 #include "ImGuiManager.h"
+#include <cassert>
 
 void Player::Initialize(Model* model, uint32_t textureHandle) {
 	// NULLポインタチェック
@@ -18,7 +18,9 @@ void Player::Initialize(Model* model, uint32_t textureHandle) {
 
 void Player::Update() {
 	// 行列を定数バッファに転送
-	worldTransform_.TransferMatrix();
+	worldTransform_.UpdateMatrix();
+
+	Rotate();
 
 	// キャラクターの移動ベクトル
 	Vector3 move = {0, 0, 0};
@@ -41,7 +43,7 @@ void Player::Update() {
 
 	// 座標移動
 	worldTransform_.translation_ = Add(worldTransform_.translation_, move);
-	
+
 	// 移動限界座標
 	const float kMoveLimitX = 34.5f;
 	const float kMoveLimitY = 19.0f;
@@ -53,7 +55,13 @@ void Player::Update() {
 	worldTransform_.translation_.y = min(worldTransform_.translation_.y, +kMoveLimitY);
 
 	worldTransform_.matWorld_ = MakeAffineMatrix(worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
-	
+
+	Attack();
+
+	// 弾更新
+	if (bullet_) {
+		bullet_->Update();
+	}
 
 	// キャラクターの座標を画面表示する処理
 	ImGui::Begin("Player");
@@ -66,5 +74,32 @@ void Player::Update() {
 void Player::Draw(ViewProjection& viewProjection) {
 	// 3Dモデルを描画
 	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+
+	// 弾描画
+	if (bullet_) {
+		bullet_->Draw(viewProjection);
+	}
 }
 
+void Player::Rotate() {
+	// 回転速さ[ラジアン/frame]
+	const float kRotSpeed = 0.02f;
+
+	// 押した方向で移動ベクトルを変更
+	if (input_->PushKey(DIK_A)) {
+		worldTransform_.rotation_.y -= kRotSpeed;
+	} else if (input_->PushKey(DIK_D)) {
+		worldTransform_.rotation_.y += kRotSpeed;
+	}
+}
+
+void Player::Attack() {
+	if (input_->PushKey(DIK_SPACE)) {
+		// 弾を生成し、初期化
+		PlayerBullet* newBullet = new PlayerBullet();
+		newBullet->Initialize(model_, worldTransform_.translation_);
+
+		// 弾を登録する
+		bullet_ = newBullet;
+	}
+}
